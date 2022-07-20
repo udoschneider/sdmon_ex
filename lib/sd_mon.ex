d623d32700baccced7625b5472495c68d0068141
Udo Schneider <Udo.Schneider@homeaddress.de>
2022-07-20 20:36:05 +0200

    Initial commit

---
+++ lib/sd_mon.ex
@@ -0,0 +1,18 @@
+defmodule SdMon do
+  @moduledoc """
+  Documentation for `SdMon`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SdMon.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sd_mon_test.exs
@@ -0,0 +1,8 @@
+defmodule SdMonTest do
+  use ExUnit.Case
+  doctest SdMon
+
+  test "greets the world" do
+    assert SdMon.hello() == :world
+  end
+end
