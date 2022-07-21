7d5ea006966d308ae9c7fbf2dbbe983d27eb0a61
Udo Schneider <Udo.Schneider@homeaddress.de>
2022-07-21 15:37:43 +0200

    Added simple Elixir wrapper

--- lib/sd_mon.ex
+++ lib/sd_mon.ex
@@ -3,16 +3,22 @@ defmodule SdMon do
   Documentation for `SdMon`.
   """
 
-  @doc """
-  Hello world.
+  def run(device) do
+    with {json, return} <- System.cmd(sdmon_path(), [device]),
+         {:ok, data} <- Jason.decode(json) do
+      case return do
+        0 -> {:ok, data}
+        _ -> {:error, data}
+      end
+    else
+      _ -> :error
+    end
+  end
 
-  ## Examples
-
-      iex> SdMon.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defp sdmon_path() do
+    :sdmon_ex
+    |> Application.app_dir()
+    |> Path.join("priv")
+    |> Path.join("sdmon")
   end
 end
